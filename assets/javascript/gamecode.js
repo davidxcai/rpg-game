f45e94b59abf077a657f297a60720c8695f2d406
Dafydd <dafyddzephyr@gmail.com>
2018-11-15 21:38:33 -0800

    added cards and jumbotron

---
+++ assets/javascript/gamecode.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    var attack = 0;
+    var healthPoints = 0;
+    var counterAtt = 0;
+    var player1;
+    var player2;
+
+    $("#david1").on("click", function() {
+        console.log("i've benn picked");
+    });
+    //grab attack and hp from card
+    //assign values to them
+    //
+    //this card = player1
+
+    //select opponent
+    //this card = player2
+
+
+});
